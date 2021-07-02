48a446cfc123e6d704ca22d3add27c5ae686d622
Justinnn07 <justinv2006@gmail.com>
2021-07-02 21:40:47 +0530

    initial commit :fire:

---
+++ components/Todo.js
@@ -0,0 +1,5 @@
+function Todo() {
+  return <div className="todo">compiled successfully</div>;
+}
+
+export default Todo;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import Head from "next/head";
+
+const myObj = [
+  {
+    name: "justin",
+    class: 10,
+    place: "MAYUR VIHAR 3",
+  },
+];
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(myObj);
 }
